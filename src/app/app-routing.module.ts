1251780ec3706b1be43c5246bc474fd810a462e5
iansu <iansucoding@hotmail.com>
2017-12-07 09:58:26 +0800

    add 404 page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeModule } from './home/home.module';
+import { PageNotFoundComponent } from './page-not-found.component';
 
 const routes: Routes = [
-  {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
   {path: 'product', loadChildren : 'app/product/product.module#ProductModule'},
   {path: 'shopping-cart', loadChildren : 'app/shopping-cart/shopping-cart.module#ShoppingCartModule'},
-  {path: 'account' , loadChildren: 'app/account/account.module#AccountModule'}
+  {path: 'account' , loadChildren: 'app/account/account.module#AccountModule'},
+  {path: '**', component : PageNotFoundComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,11 +9,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { CoreModule } from './core/core.module';
 import { ShoppingCartService } from './shopping-cart/shopping-cart.service';
 import { AccountService } from './account/account.service';
+import { PageNotFoundComponent } from './page-not-found.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    PageNotFoundComponent,
   ],
   imports: [
     BrowserModule,
